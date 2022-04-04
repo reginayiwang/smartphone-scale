3aa2fc931bd3bec0642f5366884b217250d27db3
Cameron Redovian <credovian3@gatech.edu>
2022-04-04 19:18:17 -0400

    evaluate.py compares one to another...

--- DataProcessing/evaluate.py
+++ DataProcessing/evaluate.py
@@ -2,40 +2,28 @@ import warnings
 warnings.filterwarnings('ignore')
 
 import csv
-import time
 import signals 
 import numpy as np
 import pandas as pd
 from pprint import pprint
 from argparse import ArgumentParser
-from sklearn.model_selection import cross_validate
+from sklearn.linear_model import LinearRegression, Lasso, Ridge
+from sklearn.metrics import mean_absolute_error, mean_absolute_percentage_error
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.linear_model import LinearRegression, Lasso, Ridge, ElasticNet
 
-def get_runtimes(model,feats,weights):
-    start_traintime = time.time()
-    model.fit(feats,weights)
-    end_traintime = time.time() - start_traintime
-    start_testtime = time.time()
-    _ = model.score(feats,weights)
-    end_testtime = time.time() - start_testtime
-    return end_traintime,end_testtime
-
-def evaluate(food,feats,weights,models=[]):
+def evaluate(food,train_data,test_data,models=[]):
     results = list()
     for mod in models:
-        cv_score = cross_validate(mod[1],feats,weights,cv=3,
-            scoring=['neg_mean_absolute_error','neg_mean_absolute_percentage_error'])
-        mae = cv_score['test_neg_mean_absolute_error'].mean()
-        mape = cv_score['test_neg_mean_absolute_percentage_error'].mean()
-        train_time,test_time = get_runtimes(mod[1],feats,weights)
+        preds = list()
+        mod[1].fit(*train_data)
+        preds = mod[1].predict(test_data[0])
+        mae = mean_absolute_error(test_data[1],preds)
+        mape = mean_absolute_percentage_error(test_data[1],preds)
         results.append({
             'CLASS':food,
             'MODEL':mod[0],
-            'MAE':round(-mae,4),
-            'MAPE':round(-100*mape,4),
-            'TRAIN TIME':round(train_time,6),
-            'TEST TIME':round(test_time,6)}) 
+            'MAE':round(mae,4),
+            'MAPE':round(100*mape,4)}) 
     return results
 
 def make_feats(df):
@@ -60,29 +48,31 @@ def prep_data(path='data/regina_03-29-22'):
     return {'Apples':apples,'Onions':onions,'Pears':pears,'All Food':all_food}
 
 def prep_results(fname='results.csv'):
-    fields = ['CLASS','MODEL','MAE','MAPE','TRAIN TIME','TEST TIME']
+    fields = ['CLASS','MODEL','MAE','MAPE']
     results_file = open(fname,'w')
     author = csv.DictWriter(results_file,fieldnames=fields) 
     author.writeheader()
     return author
 
 def main(args):
-    data = prep_data(args.input_dir) 
+    train_data = prep_data(args.train_data)
+    test_data = prep_data(args.test_data)
     author = prep_results(args.output_file)
-    names = ['Linear Regression','Lasso','Ridge','Elastic Net','Random Forest']
-    models = [LinearRegression(),Lasso(),Ridge(),ElasticNet(),RandomForestRegressor()]
-    for k,v in data.items():
-        count,minw,maxw = len(v[1]),v[1].min(),v[1].max()
-        results = evaluate(k,v[0],v[1],list(zip(names,models)))
+    names = ['Linear Regression','Lasso','Ridge','Random Forest']
+    models = [LinearRegression(),Lasso(),Ridge(),RandomForestRegressor()]
+    for food in train_data.keys():
+        count,minw,maxw = len(train_data[food][1]),train_data[food][1].min(),train_data[food][1].max()
+        results = evaluate(food,train_data[food],test_data[food],list(zip(names,models)))
         author.writerows(results)
         if args.verbose: 
-            print(f'\n--> Evaluating {k}')
+            print(f'--> Evaluating {food}')
             print(f'Count: {count}  Min/Max: {minw}/{maxw}')
             pprint(results)
 
 if __name__=='__main__':
     parser = ArgumentParser()
     parser.add_argument('-v','--verbose',default=False,action='store_true')
-    parser.add_argument('-i','--input_dir',default='data/all_data')
-    parser.add_argument('-o','--output_file',default='results/all_data_results.csv')
+    parser.add_argument('--train_data',default='data/regina_03-29-22')
+    parser.add_argument('--test_data',default='data/ram_04-01-22')
+    parser.add_argument('-o','--output_file',default='regina_to_ram_results.csv')
     main(parser.parse_args())
