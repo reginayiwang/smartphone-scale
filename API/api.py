eab316afaea335651e9505fd9a18c67d9b707b20
Regina Wang <rwang656@gatech.edu>
2022-04-19 19:13:53 -0700

    Fixed harminv in API and added option for original method

--- API/api.py
+++ API/api.py
@@ -1,13 +1,18 @@
 from flask import Flask, request, jsonify
 import joblib
 import traceback
+import os
 from io import TextIOWrapper
 import signals
 
-apple_model = joblib.load("Apples_RFEElasticNet_model.pkl")
-onion_model = joblib.load("Onions_RFEElasticNet_model.pkl")
-pear_model = joblib.load("Pears_RFEElasticNet_model.pkl")
-all_model = joblib.load("AllFood_RFEElasticNet_model.pkl")
+apple_model = joblib.load("models/Apple_model.pkl")
+onion_model = joblib.load("models/Onion_model.pkl")
+pear_model = joblib.load("models/Pear_model.pkl")
+all_model = joblib.load("models/AllFood_model.pkl")
+og_apple_model = joblib.load("models/OG_Apple_model.pkl")
+og_onion_model = joblib.load("models/OG_Onion_model.pkl")
+og_pear_model = joblib.load("models/OG_Pear_model.pkl")
+og_all_model = joblib.load("models/OG_AllFood_model.pkl")
 
 app = Flask(__name__)
 
@@ -21,19 +26,45 @@ def predict():
         acc_file = TextIOWrapper(request.files['acc'])
         gyro_file = TextIOWrapper(request.files['gyro'])
         food = request.form['food']
-        features = signals.make_feats(acc_file, gyro_file)
+
+        features = signals.make_feats(acc_file, gyro_file, False).reshape(1, -1)
         if food == 'Apple':
-            weight = apple_model.predict(features.reshape(1, -1))
+            weight = apple_model.predict(features)
         elif food == 'Onion':
-            weight = onion_model.predict(features.reshape(1, -1))
+            weight = onion_model.predict(features)
         elif food == 'Pear':
-            weight = pear_model.predict(features.reshape(1, -1))
+            weight = pear_model.predict(features)
         else:
-            weight = all_model.predict(features.reshape(1, -1))
+            weight = all_model.predict(features)
+
         return jsonify({'weight': str(weight)})
+
+    except:
+        return jsonify({'trace': traceback.format_exc()})
+
+@app.route('/predict_og', methods=['POST'])
+def predict_og():
+    try:
+        acc_file = TextIOWrapper(request.files['acc'])
+        gyro_file = TextIOWrapper(request.files['gyro'])
+        food = request.form['food']
+        
+        features = signals.make_feats(acc_file, gyro_file, True).reshape(1, -1)
+        if food == 'Apple':
+            weight = og_apple_model.predict(features)
+        elif food == 'Onion':
+            weight = og_onion_model.predict(features)
+        elif food == 'Pear':
+            weight = og_pear_model.predict(features)
+        else:
+            weight = og_all_model.predict(features)
+
+        return jsonify({'weight': str(weight)})
+
     except:
         return jsonify({'trace': traceback.format_exc()})
 
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
--- API/signals.py
+++ API/signals.py
@@ -1,48 +1,39 @@
-from re import X
 import scipy.signal as sp
 import scipy.interpolate as ip
-from sklearn.preprocessing import PolynomialFeatures
 import csv
 import numpy as np
-import pandas as pd
 import harminv
 
-def make_feats(acc_file, gyro_file):
-    df = pd.DataFrame.from_dict(generate_features_from_file(acc_file, gyro_file))
-    cin = df['classic_intensity']
-    fin = df['filtered_intensity']
-    lips = df['left_ips']
-    rips = df['right_ips']
-    pmag = df['peak_magnitude']
-    pfre = df['peak_frequency']
-    hmag = df['h_peak_magnitude']
-    hfre = df['h_peak_frequency']
-    poly = PolynomialFeatures(degree=3)
-    feats = poly.fit_transform(np.array(hmag).reshape(1, -1))
+def make_feats(acc_file, gyro_file, OG=False):
+    if OG:
+        feats = generate_features_from_file(acc_file, gyro_file, mode='sub', OG=True)
+        cin = feats['classic_intensity'][1]
+        return np.array(cin)
+    else:
+        xz_indices = [0, 2]
+        feats = generate_features_from_file(acc_file, gyro_file, mode='per', OG=False)
+        fin = feats['filtered_intensity'][xz_indices]
+        hmag = feats['h_peak_magnitude'][xz_indices]
+        return np.concatenate((fin, hmag), axis=0)
     
-    return np.array(feats)
-    # return np.concatenate((cin,fin,lips,rips,pmag,pfre),axis=0)
-
 def resample_and_sync(at,a,wt,w,period=1.0/396.4):
-#   period_adjusted = period * 1e9 # in microseconds
+    start_point = max(at[0],wt[0])
+    end_point = min(at[-1],wt[-1])
 
-  start_point = max(at[0],wt[0])
-  end_point = min(at[-1],wt[-1])
+    kind='cubic' # 'quadratic'
+    a_f = ip.interp1d(at,a,kind=kind)
+    w_f = ip.interp1d(wt,w,kind=kind)
 
-  kind='cubic' # 'quadratic'
-  a_f = ip.interp1d(at,a,kind=kind)
-  w_f = ip.interp1d(wt,w,kind=kind)
+    new_t = [start_point + i * period for i in range(0,1+int((end_point - start_point)/period))]
+    new_t_seconds = (new_t - start_point)*1e-9
 
-  new_t = [start_point + i * period for i in range(0,1+int((end_point - start_point)/period))]
-  new_t_seconds = (new_t - start_point)*1e-9
+    new_a_stack = a_f(new_t)
+    new_w_stack = w_f(new_t)
+    S = np.vstack((new_a_stack[0,:],new_a_stack[1,:],new_a_stack[2,:],new_w_stack[0,:],new_w_stack[1,:],new_w_stack[2,:]))
 
-  new_a_stack = a_f(new_t)
-  new_w_stack = w_f(new_t)
-  S = np.vstack((new_a_stack[0,:],new_a_stack[1,:],new_a_stack[2,:],new_w_stack[0,:],new_w_stack[1,:],new_w_stack[2,:]))
+    return new_t_seconds, 1.0/(period*1e-9), S
 
-  return new_t_seconds, 1.0/(period*1e-9), S
-
-def load_data(acc_file, gyro_file):
+def load_data(acc_file, gyro_file, OG=False):
     at,ax,ay,az = [],[],[],[]
     wt,wx,wy,wz = [],[],[],[]
 
@@ -53,7 +44,6 @@ def load_data(acc_file, gyro_file):
         ax.append(float(row[1]))
         ay.append(float(row[2]))
         az.append(float(row[3]))
-
 
     accdata = csv.reader(gyro_file, delimiter=',', quotechar='|')
     next(accdata) # first line is labels, this skips
@@ -67,14 +57,16 @@ def load_data(acc_file, gyro_file):
     a = np.vstack((np.array(ax),np.array(ay),np.array(az)))
     w = np.vstack((np.array(wx),np.array(wy),np.array(wz)))
 
-    dt_a = (at[1:] - at[:-1])
-    dt_w = (wt[1:] - wt[:-1])
+    if OG:
+        at, wt = np.array(at[::2]), np.array(wt[::2])
+        a = np.vstack((np.array(ax[::2]),np.array(ay[::2]),np.array(az[::2])))
+        w = np.vstack((np.array(wx[::2]),np.array(wy[::2]),np.array(wz[::2])))
 
-    t, fs, S = resample_and_sync(at, a, wt, w, np.median(dt_a))
-    return t, fs, S
+    dt_a = (at[1:] - at[:-1])
+    
+    return resample_and_sync(at, a, wt, w, np.median(dt_a))
 
 def filter_data(S, fs, fc_low=128, fc_high=192, plot_freq_response=False):
-    # filters data w/ highpass or bandpass filter (if fc_high is not None)  
     if fc_high is not None:
         sos = sp.butter(1, [2*fc_low/fs,2*fc_high/fs], 'bandpass', analog=False,output='sos')
     else:
@@ -83,7 +75,6 @@ def filter_data(S, fs, fc_low=128, fc_high=192, plot_freq_response=False):
     return sp.sosfilt(sos, S, axis=1)
 
 def segment_signal(t, S, ref_bounds, obj_bounds):
-    # bounds format: (start, stop) in seconds
     ref_start, ref_stop = np.argwhere(t<=ref_bounds[0])[-1][0] if ref_bounds[0] > 0 else 0, np.argwhere(t<=ref_bounds[1])[-1][0]
     obj_start, obj_stop = np.argwhere(t<=obj_bounds[0])[-1][0], np.argwhere(t<=obj_bounds[1])[-1][0]
     
@@ -93,51 +84,23 @@ def segment_signal(t, S, ref_bounds, obj_bounds):
         ref_stop = ref_start + obj_len
     elif ref_len < obj_len:
         obj_stop = obj_start + ref_len
-    # we should probably enforce the same size if we want to output this raw or if we want to output the full fft to the ml algs
 
-    return t[ref_start:ref_stop], t[obj_start:obj_stop], S[:, ref_start:ref_stop], S[:, obj_start:obj_stop]
+    return S[:, ref_start:ref_stop], S[:, obj_start:obj_stop]
 
-def peak_data(fs, S):
-    PSD = np.abs(np.fft.fft(S,axis=1))**2
-    PSD = PSD[:,:PSD.shape[1]//2] # np.log(1+PSD[:,:PSD.shape[1]//2]) #work in log?
-    peak_freqs, peak_mags, left_ips, right_ips = np.empty(6), np.empty(6), np.empty(6), np.empty(6), 
-
-    f = np.linspace(0,fs/2,PSD.shape[1])
-
-    for i in range(6):
-        peaks, peaks_dict = sp.find_peaks(PSD[i,:],height=(0,None),width=(None,None),plateau_size=(None,None))
-        peak_arg = np.argmax(peaks_dict['peak_heights'])
-        peak_freqs[i] = f[peaks[peak_arg]]
-        peak_mags[i] = peaks_dict['peak_heights'][peak_arg]
-        left_ips[i] = peaks_dict['left_ips'][peak_arg]
-        right_ips[i] = peaks_dict['right_ips'][peak_arg]
-
-
-    return peak_freqs, peak_mags, left_ips, right_ips
-    
 def harminv_peaks(fs, S):
-    h_peak_freqs, h_peak_mags, h_decay, h_Q, h_error = np.empty(6), np.empty(6), np.empty(6), np.empty(6), np.empty(6)
+    h_peak_mags = np.empty(6)
     for i in range(6):
         inversion = harminv.invert(S[i,:], fmin=fs*5./16., fmax=fs*7./16., dt=1.0/fs, nf=64)
         best = np.argmax([inversion[j][1] for j in range(inversion.shape[0])])
-        h_peak_freqs[i] = inversion[best][0]
         h_peak_mags[i] = inversion[best][1]
-        h_decay[i] = inversion[best][3]
-        h_Q[i] = inversion[best][4]
-        h_error[i] = inversion[best][5]
 
-    return h_peak_freqs, h_peak_mags, h_decay, h_Q, h_error
+    return h_peak_mags
 
 def classic_intensity(S):
-    #original vibroscale feature
     offset = S - np.expand_dims(np.mean(S, axis=1), axis=-1)
     intensity = np.sum(np.abs(offset), axis=1)/S.shape[1]
 
     return intensity
-
-def classic_power(S):
-    offset = S - np.expand_dims(np.mean(S, axis=1), axis=-1)
-    return np.sum(np.abs(offset)**2, axis=1)/S.shape[1]
 
 def compare_refs(ref, obj, mode):
     if mode == 'div':
@@ -149,68 +112,29 @@ def compare_refs(ref, obj, mode):
 
     return (ref, obj)
 
-def generate_features(t,fs,S,ref_bounds,obj_bounds, mode='div', Harminv=True):
+def generate_features(t,fs,S,ref_bounds,obj_bounds, mode, OG=False):
     feature_dict = {}
     
-    ref_t, obj_t, ref_S, obj_S = segment_signal(t, S, ref_bounds,obj_bounds)
+    ref_S, obj_S = segment_signal(t, S, ref_bounds,obj_bounds)
 
     filtered_S = filter_data(S, fs, fs/4., None)
-    ref_t, obj_t, filtered_ref_S, filtered_obj_S  = segment_signal(t, filtered_S, ref_bounds,obj_bounds)
+    filtered_ref_S, filtered_obj_S  = segment_signal(t, filtered_S, ref_bounds,obj_bounds)
 
-    feature_dict['classic_intensity'] = compare_refs(classic_intensity(ref_S), classic_intensity(obj_S), mode) # ignore in favor of filtered_classic?
+    if OG:     
+        feature_dict['classic_intensity'] = compare_refs(classic_intensity(ref_S), classic_intensity(obj_S), 'sub') # ignore in favor of filtered_classic?
+        return feature_dict
+
     feature_dict['filtered_intensity'] = compare_refs(classic_intensity(filtered_ref_S), classic_intensity(filtered_obj_S), mode)
 
-    #Shouldn't need to filter peak-finder data because we're in frequency space anyways\    
-    ref_peak_freqs, ref_peak_mags, ref_left_ips, ref_right_ips = peak_data(fs, ref_S)
-    obj_peak_freqs, obj_peak_mags, obj_left_ips, obj_right_ips = peak_data(fs, obj_S)
+    ref_h_peak_mags = harminv_peaks(fs, ref_S)
+    obj_h_peak_mags = harminv_peaks(fs, obj_S)
 
-    feature_dict['peak_frequency'] = compare_refs(ref_peak_freqs, obj_peak_freqs, mode)
-    feature_dict['peak_magnitude'] = compare_refs(ref_peak_mags, obj_peak_mags, mode)
-    feature_dict['left_ips'] = compare_refs(ref_left_ips, obj_left_ips, mode)
-    feature_dict['right_ips'] = compare_refs(ref_right_ips, obj_right_ips, mode)
-
-    if Harminv:
-        ref_h_peak_freqs, ref_h_peak_mags, ref_h_decay, ref_h_Q, ref_h_error = harminv_peaks(fs, ref_S)
-        obj_h_peak_freqs, obj_h_peak_mags, obj_h_decay, obj_h_Q, obj_h_error = harminv_peaks(fs, obj_S)
-
-        feature_dict['h_peak_frequency'] = compare_refs(ref_h_peak_freqs, obj_h_peak_freqs, mode)
-        feature_dict['h_peak_magnitude'] = compare_refs(ref_h_peak_mags, obj_h_peak_mags, mode)
-        feature_dict['h_decay'] = compare_refs(ref_h_decay, obj_h_decay, mode)
-        feature_dict['h_Q'] = compare_refs(ref_h_Q, obj_h_Q, mode)
-        feature_dict['h_error'] = compare_refs(ref_h_error, obj_h_error, mode)
+    feature_dict['h_peak_magnitude'] = compare_refs(ref_h_peak_mags, obj_h_peak_mags, mode)
     
     return feature_dict
 
-def generate_features_from_file(acc_file, gyro_file, ref_bounds= (0.1,2.9), obj_bounds = (5.1,7.9), mode='sub'):
-    """
-    main feature generator function for vibroscale
-        inputs:
-            filename: name of accelerometer csv (matching gyroscope data will be accessed automatically)
-            ref bounds = ( start time in seconds, stop time in seconds) of no-object-on-phone-vibration
-            obj bounds = ( start time in seconds, stop time in seconds) of object-on-phone-vibration
-            mode = 'div', 'sub', 'per', or None :
-                'div' -> object_feature/ref_feature
-                'sub' -> object_feature-ref_feature
-                'per' -> (object_featur-ref_feature)/ref_feature
-                None -> return object features only (only data from vibration with object on phone)
-            
-            outputs: 
-            feature_dict:
-                6-dimensional vectors referring to the xyz axis of the accelerometer and then the gyroscope:
-                    feature_dict['classic_intensity'] : original vibroscale calculation, no filtering
-                    feature_dict['filtered_intensity'] : original vibroscale calculation, filtered around signal
-                    feature_dict['peak_frequency'] : frequency of largest peak in fft
-                    feature_dict['peak_magnitude'] : magnitude of largest peak in fft
-                    feature_dict['left_ips] : left kurtosis, ish (intersection of interpolated peak w/ base)
-                    feature_dict['right_ips] : right kurtosis, ish (intersection of interpolated peak w/ base)
-                1-dim vectors/descriptors
-                    feature_dict['weight'] : int, weight in grams, from filename
-                    feature_dict['class'] : str, type of fruit, from filename
-            
-            all features are 6-dimensional vectors referring to the xyz axis of the accelerometer and then the gyroscope
-    """
-    t, fs, S = load_data(acc_file, gyro_file)
-
-    feature_dict = generate_features(t,fs,S,ref_bounds,obj_bounds, mode)
-
+def generate_features_from_file(acc_file, gyro_file, ref_bounds= (0.1,2.9), obj_bounds = (5.1,7.9), mode='sub', OG=False):
+    t, fs, S = load_data(acc_file, gyro_file, OG=OG)
+    
+    feature_dict = generate_features(t,fs,S,ref_bounds,obj_bounds, mode, OG=OG)
     return feature_dict
